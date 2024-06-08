05cc235d38726276e548b0b8c66f713a84c45309
Nuttaporn21 <33923684+Nuttaporn21@users.noreply.github.com>
2024-06-08 14:52:43 +0700

    Add files via upload

---
+++ exfor.py
@@ -0,0 +1,14 @@
+# loop through string
+subj = 'Python&AI'
+for n in subj:
+    print(n)
+
+# loop through list
+names = ['Manee', 'Mana', 'Chujai', 'Piti', 'Malinee']
+for n in names:
+    print(n)
+
+# loop through range
+for n in range(0, 30, 5):
+    print(n, end = ' ')
+
